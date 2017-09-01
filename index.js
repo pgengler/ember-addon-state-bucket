e481c7228027822f6c2fd390cc237358a6bcb000
Curtis Ekstrom <code@fauxton.io>
2017-09-01 22:17:01 +0200

    Replace EventEmitter with node-persist

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 /* eslint-env node */
 'use strict';
+const storage = require('node-persist');
 
 module.exports = {
   name: 'ember-addon-state-bucket',
 
   included() {
-    this.emberAddonStateBucket = {
-      state: {}
-    };
-
-    process.on('ember-addon-state-bucket:get', (key) => {
-      let { state } = this.emberAddonStateBucket;
-      return state[key];
-    });
-
-    process.on('ember-addon-state-bucket:set', (key, value) => {
-      let { state } = this.emberAddonStateBucket;
-      return state[key] = value;
-    });
-
-    process.on('ember-addon-state-bucket:push', (key, value) => {
-      let { state } = this.emberAddonStateBucket;
-      if (state[key] instanceof Array) {
-        return state[key].push(value);
-      } else if (state[key] instanceof Set) {
-        return state[key].add(value);
-      }
-    });
+    storage.initSync();
+    process.emberAddonStateBucket = storage;
   },
 };
